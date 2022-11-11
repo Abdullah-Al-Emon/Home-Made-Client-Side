a118fe6850637614e866a498bc3109c44c4967ad
abdullah al emon <abdullahemon905@gmail.com>
2022-11-11 17:56:16 +0600

    see all details services section complete

--- src/Pages/Home/Services/Services.js
+++ src/Pages/Home/Services/Services.js
@@ -35,7 +35,6 @@ const Services = () =>
                 </div>
             </div>
             <div className='grid gap-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 my-8'>
-                
                 {
                     services.map(service => <ServicesCard
                         key={service._id}
